131cc00ee26296514afa45b6087533d3c68b40ef
cks3066 <cks3066@naver.com>
2023-03-05 17:52:52 +0900

    chore: query client 사용 방식 변경 - app 디렉토리 방식에 따라 불필요한 dehydrate 과정 제거

--- src/lib/providers/ReactQuery.tsx
+++ src/lib/providers/ReactQuery.tsx
@@ -1,24 +1,17 @@
 "use client";
 
-import { QueryClient, QueryClientProvider, Hydrate } from "@tanstack/react-query";
-import { PropsWithChildren, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PropsWithChildren } from "react";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 2 * 60 * 1000,
+      suspense: true,
+    },
+  },
+});
 
 export default function ReactQuery({ children }: PropsWithChildren) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 2 * 60 * 1000,
-            suspense: true,
-          },
-        },
-      })
-  );
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate>{children}</Hydrate>
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
