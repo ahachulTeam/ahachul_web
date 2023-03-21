cc4539a8dc77db9ad51791106fe248d1725c41b2
jtwjs <xodnd9503@gmail.com>
2023-03-21 22:00:17 +0900

    feat: 라우트 패스 경로 상수 추가

---
+++ src/constants/path.ts
@@ -0,0 +1,9 @@
+export const PATH = {
+  LOGIN: "/login",
+  SIGN_UP: "/signup",
+  HOME: "/home",
+  LOST_FOUND: "/lostFound",
+  BOARD: "/board",
+  LABS: "/labs",
+  COMPLAINTS: "/complaints",
+};
