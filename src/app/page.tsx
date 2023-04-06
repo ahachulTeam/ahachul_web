e6e1e8e8c490184aee23d9d649b2bbabdc20cc00
createhb21 <alsghk9701@gmail.com>
2023-04-06 21:45:50 +0900

    fix: fix for build err

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <Link href="/onboarding">
-      <button type="button">onboarding</button>
-    </Link>
+    <>
+      <Link href="/onboarding">
+        <button type="button">onboarding</button>
+      </Link>
+      <Link href="/samplePage">
+        <button type="button">samplePage</button>
+      </Link>
+    </>
   );
 }
