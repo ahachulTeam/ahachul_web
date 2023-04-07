16a379b8301e61c64bf4556623b3a6460211b324
cks3066 <cks3066@naver.com>
2023-04-07 22:21:06 +0900

    feat: 아이콘 적용

--- src/hooks/useToast.tsx
+++ src/hooks/useToast.tsx
@@ -1,16 +1,19 @@
+import Image from "next/image";
 import { toast, ToastOptions } from "react-toastify";
+
+import { Notification } from "@/assets/icons";
 
 const useToast = () => {
   const success = (content: React.ReactNode, option?: ToastOptions) => {
     toast.success(content, {
-      // icon: <Image src="/images/??" width={20} height={20} />,
+      icon: false,
       ...option,
     });
   };
 
   const error = (content: React.ReactNode, option?: ToastOptions) => {
     toast.error(content, {
-      // icon: <Image src="/images/??" width={20} height={20} />,
+      icon: <Notification />,
       ...option,
     });
   };
