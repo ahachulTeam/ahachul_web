179af910822c2a1168386474173422a23c803f58
jtwjs <xodnd9503@gmail.com>
2023-04-08 15:25:58 +0900

    feat: 토글 컴포넌트 스타일 수정

--- src/styles/themes/components/toggle.ts
+++ src/styles/themes/components/toggle.ts
@@ -15,9 +15,9 @@ export const toggle = {
     height: 36px;
     padding: 0 20px;
     color: ${colors.gray_40};
-    background-color: ${colors.gray_20};
     user-select: none;
     transition: all 0.3s ease-in-out;
+    z-index: 2;
 
     &:first-child {
       border-top-left-radius: 40px;
@@ -29,22 +29,8 @@ export const toggle = {
       border-bottom-right-radius: 40px;
     }
 
-    @media (hover: hover) {
-      &:not(:disabled):hover {
-        color: ${colors.white};
-        background-color: ${colors.primary};
-
-        &:not([data-state="active"]) {
-          color: ${colors.white};
-          background-color: ${colors.primary_hover};
-        }
-      }
-    }
-
     &[data-state="active"] {
       color: ${colors.white};
-      background-color: ${colors.primary};
-      font-weight: 600;
     }
   `,
 } as const;
