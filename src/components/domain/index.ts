d3765470f8afc9d1051e6bd3ce2b3a2f6142eed5
jtwjs <xodnd9503@gmail.com>
2023-05-06 18:17:58 +0900

    feat: FilterList 컴포넌트, 유실물 필터 static 모듈 추가

--- src/components/domain/index.ts
+++ src/components/domain/index.ts
@@ -1,2 +1,3 @@
 export * from "./main";
 export * from "./onboarding";
+export * from "./lostFound";
---
+++ src/components/domain/lostFound/filterList/styled.ts
@@ -0,0 +1,6 @@
+import { styled } from "styled-components";
+
+export const FilterList = styled.div`
+  display: flex;
+  column-gap: 8px;
+`;
