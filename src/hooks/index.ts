82e83038271f56c9f22ddb1f86aa0402d28bc907
jtwjs <xodnd9503@gmail.com>
2023-05-06 18:39:13 +0900

    feat: 불필요한 파일 제거

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,3 @@
-export { default as useSample } from "./useSample";
 export { default as useToast } from "./useToast";
 export { default as useDisclosure } from "./useDisclosure";
 export { default as useScrollDirection } from "./useScrollDirection";
