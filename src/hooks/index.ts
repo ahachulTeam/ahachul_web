4664b386f39c489e72aecf7b2580a82e7ff98f65
jtwjs <xodnd9503@gmail.com>
2023-05-06 18:13:02 +0900

    feat: 접근성 관련 키보드 인터렉션 hooks 추가

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,5 @@ export { default as useSample } from "./useSample";
 export { default as useToast } from "./useToast";
 export { default as useDisclosure } from "./useDisclosure";
 export { default as useScrollDirection } from "./useScrollDirection";
+export { default as useKeyTrap } from "./useKeyTrap";
+export { default as useArrowKeyTrap } from "./useArrowKeyTrap";
