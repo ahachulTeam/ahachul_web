24e63b926d9445baac5342858237ae35f5b1d8e6
createhb21 <alsghk9701@gmail.com>
2023-05-12 03:10:14 +0900

    chore: babel => swc 설정

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   compress: true,
+  compiler: {
+    emotion: true,
+  },
+  experimental: {
+    appDir: false,
+    forceSwcTransforms: true,
+  },
+  swcMinify: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
