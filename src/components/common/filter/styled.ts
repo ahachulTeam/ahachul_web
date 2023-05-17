14f895557ed82e7d9bcfde88a8d694882df6dc6c
jtwjs <xodnd9503@gmail.com>
2023-05-17 17:04:36 +0900

    feat: aria-selected 접근성 추가

--- src/components/common/filter/styled.ts
+++ src/components/common/filter/styled.ts
@@ -11,24 +11,27 @@ export const TriggerBtn = styled.button`
     width: 81px;
     height: 29px;
     border-radius: 65px;
-    color: ${theme.colors.gray_40};
+    color: ${theme.colors.gray_55};
     background-color: ${theme.colors.gray_20};
     transition: all 0.3s ease-in-out;
 
     & > svg {
-      stroke: ${theme.colors.gray_40};
+      stroke: ${theme.colors.gray_55};
       transform: rotate(0);
       transition: all 0.3s ease-in-out;
     }
 
-    &[aria-expanded="true"] {
+    &[aria-expanded="true"],
+    &[aria-selected="true"] {
       color: ${theme.colors.primary};
       background-color: ${theme.colors.secondary};
-
       & > svg {
         stroke: ${theme.colors.primary};
-        transform: rotate(180deg);
       }
+    }
+
+    &[aria-expanded="true"] > svg {
+      transform: rotate(180deg);
     }
   `}
 `;
