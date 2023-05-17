edf0487cc3cf12d6ae46670d1cfa92c7c65a9691
jtwjs <xodnd9503@gmail.com>
2023-05-17 17:28:16 +0900

    feat: 네브바 애니메이션 변경

--- src/components/layout/navbar/Navbar.styled.ts
+++ src/components/layout/navbar/Navbar.styled.ts
@@ -9,16 +9,15 @@ export const Navbar = styled.nav`
     max-width: ${theme.size.layout.width};
     width: 100%;
     height: ${theme.size.bottomNavbar.height};
-    max-height: 0;
     border-top: 1px solid ${theme.colors.gray_30};
     background-color: ${theme.colors.white};
-    transform: translateX(-50%);
-    transition: max-height 500ms cubic-bezier(0.43, 0.03, 0.15, 0.95);
+    transform: translate(-50%, 100%);
+    transition: transform 400ms cubic-bezier(0.43, 0.03, 0.15, 0.95);
     overflow: hidden;
     z-index: ${theme.zIndex.sticky};
 
     &[data-show="true"] {
-      max-height: ${theme.size.bottomNavbar.height};
+      transform: translate(-50%, 0);
     }
   `}
 `;
