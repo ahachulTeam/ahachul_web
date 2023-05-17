b47a7471d6e854477df4bf7f680c47a7be948b6e
jtwjs <xodnd9503@gmail.com>
2023-05-17 16:41:04 +0900

    feat: carusel 공용 컴포넌트로 분리

---
+++ src/components/common/carousel/styled.ts
@@ -0,0 +1,38 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+export const CarouselLayout = styled.div`
+  ${({ theme }) => css`
+    position: relative;
+
+    .swiper {
+      position: relative;
+      padding: 4px 8px;
+    }
+
+    .swiper-pagination {
+      position: absolute;
+      left: 50%;
+      bottom: 0;
+      display: flex !important;
+      justify-content: center;
+      width: 100%;
+      height: max-content;
+      padding: 30px 0 35px;
+      background: ${theme.colors.white};
+      transform: translateX(-50%);
+      z-index: ${theme.zIndex.sticky};
+    }
+
+    .swiper-pagination-bullet {
+      background: ${theme.colors.gray_23};
+      transition: all 0.3s linear;
+
+      &-active {
+        width: 23px;
+        border-radius: 37px;
+        background: ${theme.colors.primary};
+      }
+    }
+  `}
+`;
