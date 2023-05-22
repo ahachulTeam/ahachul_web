b458bf48fe95a1ad867d9d3583789f20df16a4f2
jtwjs <xodnd9503@gmail.com>
2023-05-23 00:46:00 +0900

    fix: resolve conflict between storybook and svgr (#69)

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,9 +1,8 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
-import svgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [react(), tsconfigPaths()],
 })
