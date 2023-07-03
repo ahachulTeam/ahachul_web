2b4d6c7aa836868514c86ab98f4d44226ecef6e1
createhb21 <alsghk9701@gmail.com>
2023-07-04 00:13:13 +0900

    feat: add few initial Modal contents UI (#98)

--- apps/web/src/components/domain/complaint/drawer/complaintDrawer/ComplaintDrawer.styled.ts
+++ apps/web/src/components/domain/complaint/drawer/complaintDrawer/ComplaintDrawer.styled.ts
@@ -1,3 +1,45 @@
+import { css } from '@emotion/react'
 import styled from '@emotion/styled'
 
-export const ComplaintDrawer = styled.div``
+export const Container = styled.section`
+  width: 100%;
+  min-height: 100%;
+`
+
+export const SectionTitle = styled.h3`
+  ${({ theme }) => css`
+    ${theme.fonts.bold16};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 32px;
+    margin-bottom: 24px;
+  `}
+`
+
+export const 지하철정보 = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 16px;
+    margin-bottom: 10px;
+
+    & > span {
+      ${theme.fonts.bold14};
+
+      & > b {
+        color: #fe8a39;
+      }
+    }
+
+    & > p {
+      ${theme.fonts.regular12};
+    }
+  `}
+`
+
+export const 진짜콘텐츠 = styled.article`
+  border-top: 1px solid #e7e7e7;
+`
--- apps/web/src/components/domain/complaint/drawer/complaintDrawer/ComplaintDrawer.tsx
+++ apps/web/src/components/domain/complaint/drawer/complaintDrawer/ComplaintDrawer.tsx
@@ -1,8 +1,19 @@
 import { css } from '@emotion/react'
 import { useEffect, useState } from 'react'
 import { useSwipeable } from 'react-swipeable'
+import * as S from './ComplaintDrawer.styled'
 import { BottomSheetWithMotion } from '@/components/common'
 import { ComplaintTargets } from '@/types/complaint'
+
+const ComplaintContents = {
+  facilities: '시설 · 환경민원',
+  temperature: '온도조절',
+  announcement: '안내방송',
+  impediment: '질서저해',
+  patient: '응급환자',
+  sexual: '성추행',
+  violence: '폭력',
+} as const
 
 interface ComplaintDrawerProps {
   isOpen: boolean
@@ -23,14 +34,16 @@ export default function ComplaintDrawer({ isOpen, targetSection, onClose }: Comp
 
   return (
     <BottomSheetWithMotion isMounted={isOpen} onClickOutside={onClose}>
-      <div
-        {...swipeHandlers}
-        css={css`
-          min-height: 400px;
-        `}
-      >
-        {selectedTab}
-      </div>
+      <S.Container {...swipeHandlers}>
+        <S.SectionTitle>{ComplaintContents[selectedTab]}</S.SectionTitle>
+        <S.지하철정보>
+          <span>
+            <b>3호선</b> 1192열차 311227호차
+          </span>
+          <p>대화행</p>
+        </S.지하철정보>
+        <S.진짜콘텐츠></S.진짜콘텐츠>
+      </S.Container>
     </BottomSheetWithMotion>
   )
 }
