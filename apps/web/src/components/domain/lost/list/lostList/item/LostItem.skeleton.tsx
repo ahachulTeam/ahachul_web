52cbac1030a459b5817cf5a83f43a37a29d4b90a
jtwjs <xodnd9503@gmail.com>
2023-07-06 15:54:17 +0900

    feat: 유실물 아이템 스켈레톤 추가 (#87)

---
+++ apps/web/src/components/domain/lost/list/lostList/item/LostItem.skeleton.tsx
@@ -0,0 +1,43 @@
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
+import * as S from './styled'
+import { View } from '@/atoms/view'
+
+interface LostItemSkeletonProps {
+  view?: View
+  count?: number
+}
+
+export default function LostItemSkeleton({ view = 'list', count = 12 }: LostItemSkeletonProps) {
+  return (
+    <>
+      {Array.from({ length: count }, (_, i) => (
+        <li key={i}>
+          <S.LostItem data-view={view}>
+            <S.Thumbnail>
+              <Skeleton containerClassName="skeleton" />
+            </S.Thumbnail>
+            <S.Contents>
+              <S.Title>
+                <Skeleton containerClassName="skeleton" height={19} />
+              </S.Title>
+              <S.Content>
+                <Skeleton containerClassName="skeleton" height={32} />
+              </S.Content>
+              <S.Meta>
+                <S.Metadata>
+                  <Skeleton containerClassName="skeleton" width={28} />
+                  <Skeleton containerClassName="skeleton" width={28} />
+                </S.Metadata>
+                <S.Utils>
+                  <Skeleton containerClassName="skeleton" width={24} />
+                </S.Utils>
+              </S.Meta>
+            </S.Contents>
+          </S.LostItem>
+        </li>
+      ))}
+    </>
+  )
+}
--- apps/web/src/components/domain/lost/list/lostList/item/LostItem.tsx
+++ apps/web/src/components/domain/lost/list/lostList/item/LostItem.tsx
@@ -1,3 +1,4 @@
+import { getCurrentTime } from '@ahhachul/lib'
 import { Badge } from '@ahhachul/ui'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -5,10 +6,9 @@ import Link from 'next/link'
 import * as S from './styled'
 import { CommentIcon } from '@/assets/icons'
 
+import type { View } from '@/atoms/view'
 import { PATH } from '@/constants/path'
 import { LostPost, LostStatus } from '@/types/lost'
-
-type View = 'list' | 'grid'
 
 interface LostItemProps {
   view?: View
@@ -22,18 +22,18 @@ export default function LostItem({ view = 'list', post }: LostItemProps) {
   return (
     <S.LostItem data-view={view}>
       <S.Thumbnail>
-        <Image src={post.imgUrl ?? '/images/default_thumbnail.svg'} fill alt="lost item thumbnail" />
+        <Image src={post.imgUrl || '/images/default_thumbnail.svg'} fill alt="lost item thumbnail" />
       </S.Thumbnail>
       <S.Contents>
         <S.Title>
-          {post.status === LostStatus.COMPLETE && <Badge label="찾기 완료" />}
+          {post.status === LostStatus.COMPLETE && <Badge variant="찾기 완료" />}
           <h3>{post.title}</h3>
         </S.Title>
         <S.Content>{post.content}</S.Content>
         <S.Meta>
           <S.Metadata>
-            <span>{post.subwayLine}</span>
-            <time>{post.createdBy}</time>
+            <span>{post.subwayLine}호선</span>
+            <time>{getCurrentTime(post.date)}</time>
           </S.Metadata>
           <S.Utils>
             <S.UtilBtn type="button" aria-label="comment" onClick={handleCommentClick}>
