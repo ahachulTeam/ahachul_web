b4a9fa76730e33812d5d921ae4b827983788af28
createhb21 <alsghk9701@gmail.com>
2023-07-18 19:38:38 +0900

    feat: build err fix (#103)

--- apps/web/src/components/domain/community/detail/header/CommunityDetailHeader.tsx
+++ apps/web/src/components/domain/community/detail/header/CommunityDetailHeader.tsx
@@ -3,7 +3,7 @@ import { Header } from '@/components/layout'
 export const CommunityDetailHeader = () => {
   return (
     <Header title="자유게시판" hasGoBack>
-      <Header.Share onClick={() => console.log('share')} />
+      <Header.Share />
       <Header.MiniHamburger onClick={() => console.log('share')} />
     </Header>
   )
