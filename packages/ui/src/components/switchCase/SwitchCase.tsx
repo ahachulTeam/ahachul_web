e181f6101ef8542752beeba8ceaf7be137243d6a
cks3066 <cks3066@naver.com>
2023-07-19 01:21:05 +0900

    refactor: SwitchCase Component 수정

--- packages/ui/src/components/switchCase/SwitchCase.tsx
+++ packages/ui/src/components/switchCase/SwitchCase.tsx
@@ -5,5 +5,9 @@ export default function SwitchCase<Case extends string>({
   caseBy,
   defaultComponent = null,
 }: SwitchCaseProps<Case>) {
+  if (value == null) {
+    return defaultComponent
+  }
+
   return caseBy[value] ?? defaultComponent
 }
