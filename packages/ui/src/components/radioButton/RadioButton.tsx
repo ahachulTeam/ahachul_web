aa7b2dd62773b7347d5734357f0b4327a249795e
cks3066 <cks3066@naver.com>
2023-07-19 01:29:33 +0900

    refactor: 에러 메시지 수정

--- packages/ui/src/components/radioButton/RadioButton.tsx
+++ packages/ui/src/components/radioButton/RadioButton.tsx
@@ -24,10 +24,10 @@ export default function RadioButton({ options, onOptionChange }: Props) {
 
   useEffect(() => {
     if (!ENABLE_OPTION_COUNT.some(item => item === options.length)) {
-      throw new Error('라디오 버튼 옵션의 개수는 2, 3, 4, 6개만 가능합니다.')
+      throw new Error('Error at <RadioButton />: 라디오 버튼 옵션의 개수는 2, 3, 4, 6개만 가능합니다.')
     }
     if (options.length !== new Set(options.map(option => option.key)).size) {
-      throw new Error('라디오 버튼 옵션의 key는 중복될 수 없습니다.')
+      throw new Error('Error at <RadioButton />: 라디오 버튼 옵션의 key는 중복될 수 없습니다.')
     }
   }, [options])
 
