bcde86f6ff15e61980416e0d93038f5a1e4189b3
cks3066 <cks3066@naver.com>
2023-07-18 21:21:20 +0900

    feat: 안내 방송 컨텐츠 생성

--- apps/web/src/components/domain/complaint/drawer/complaintDrawer/contents/Announcement.tsx
+++ apps/web/src/components/domain/complaint/drawer/complaintDrawer/contents/Announcement.tsx
@@ -1,3 +1,5 @@
+import { fonts } from '@ahhachul/design-system'
+import { Button } from '@ahhachul/ui'
 import { css } from '@emotion/react'
 
 export default function Announcement() {
@@ -5,11 +7,40 @@ export default function Announcement() {
     <div
       css={css`
         display: flex;
+        gap: 15px;
         flex-direction: column;
+        padding: 20px;
       `}
     >
-      <div>방송음량</div>
-      <div>요청</div>
+      <div></div>
+      <div>
+        <div
+          css={css`
+            ${fonts.medium14};
+            display: flex;
+            align-items: center;
+            height: 30px;
+            width: 100%;
+          `}
+        >
+          방송음량
+        </div>
+        <Button size="lg" variant="secondary" label="너무 커요" />
+      </div>
+      <div>
+        <div
+          css={css`
+            ${fonts.medium14};
+            display: flex;
+            align-items: center;
+            height: 30px;
+            width: 100%;
+          `}
+        >
+          요청
+        </div>
+        <Button size="lg" variant="secondary" label="행선 안내방송" />
+      </div>
     </div>
   )
 }
