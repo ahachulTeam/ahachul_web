4f312adba6e322ab7303c82ce436e398128595b7
jtwjs <xodnd9503@gmail.com>
2023-07-23 18:11:55 +0900

    feat: 플로팅 버튼 z-index 추가

--- apps/web/src/components/common/floatingButton/styled.ts
+++ apps/web/src/components/common/floatingButton/styled.ts
@@ -8,4 +8,5 @@ export const floatCss = (theme: Theme) => css`
   left: calc(100% - 16px - 132px); // fixme
   padding: 0 30px;
   filter: drop-shadow(0px 6px 16px rgba(91, 91, 91, 0.25));
+  z-index: ${theme.zIndex.sticky};
 `
