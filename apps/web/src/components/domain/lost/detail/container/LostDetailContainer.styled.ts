5e86ba3b82799a50d4c8fc3ab5da6aa66bef5238
jtwjs <xodnd9503@gmail.com>
2023-07-23 18:11:22 +0900

    feat: 유실물 상세 컨테이너 컴포넌트 추가 (#46)

--- apps/web/src/components/domain/lost/detail/container/LostDetailContainer.styled.ts
+++ apps/web/src/components/domain/lost/detail/container/LostDetailContainer.styled.ts
@@ -3,7 +3,6 @@ import styled from '@emotion/styled'
 
 export const Section = styled.section`
   ${({ theme }) => css`
-    padding: 19px 0;
     background-color: ${theme.colors.white};
   `}
 `
--- apps/web/src/components/domain/lost/detail/container/LostDetailContainer.tsx
+++ apps/web/src/components/domain/lost/detail/container/LostDetailContainer.tsx
@@ -1,7 +1,22 @@
-// import { Carousel } from '@/components/common'
+import { LostDetailContents } from '../contents'
+import { OtherLostFounds } from '../otherLostFounds'
+import { StickyArea } from '../stickyArea'
+import * as S from './LostDetailContainer.styled'
+import { useOwnArticle } from '@/hooks'
 
-import * as S from './styled'
+interface LostDetailContainerProps {
+  createdBy: string
+  subwayLine: number
+}
 
-export default function LostDetailContainer() {
-  return <S.Section>setion</S.Section>
+export default function LostDetailContainer({ createdBy, subwayLine }: LostDetailContainerProps) {
+  const isOwner = useOwnArticle(createdBy)
+
+  return (
+    <S.Section>
+      <LostDetailContents />
+      <OtherLostFounds lostType="ACQUIRE" subwayLine={subwayLine} />
+      {!isOwner && <StickyArea />}
+    </S.Section>
+  )
 }
