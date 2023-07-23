6af252d949682e6afe5166ea7f01958d0c5e23a5
jtwjs <xodnd9503@gmail.com>
2023-07-23 18:11:38 +0900

    feat: 유실물 상세 페이지 컴포넌트 추가 (#46)

--- apps/web/pages/lost/[id].tsx
+++ apps/web/pages/lost/[id].tsx
@@ -1,12 +1,69 @@
+import { QueryClient } from '@tanstack/react-query'
+import { GetServerSideProps } from 'next'
 import { type ReactElement } from 'react'
 
+import lostAPI from '@/apis/lost'
 import { LostDetailContainer, LostDetailHeader } from '@/components/domain'
 import { Layout } from '@/components/layout'
+import { PATH } from '@/constants'
+import { SEO } from '@/libs'
+import { lostKeys } from '@/queries/lost/keys'
+import * as T from '@/utils/try'
 
-export default function LostDetailPage() {
-  return <LostDetailContainer />
+interface LostDetailPageProps {
+  title: string
+  content: string
+  createdBy: string
+  subwayLine: number
+}
+export default function LostDetailPage({ title, content, createdBy, subwayLine }: LostDetailPageProps) {
+  return (
+    <>
+      <SEO title={title} description={content} />
+      <LostDetailContainer createdBy={createdBy} subwayLine={subwayLine} />
+    </>
+  )
 }
 
 LostDetailPage.getLayout = function getLayout(page: ReactElement) {
   return <Layout Header={<LostDetailHeader />}>{page}</Layout>
+}
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const { params } = context
+  const lostId = params?.id as string
+
+  if (!lostId) {
+    return {
+      redirect: {
+        destination: PATH.HOME,
+        permanent: true,
+      },
+    }
+  }
+
+  const queryClient = new QueryClient()
+
+  const res = await queryClient.fetchQuery(lostKeys.detail(lostId), () => lostAPI.fetchLostDetail(lostId))
+  const parseData = T.parseResponse(res)
+
+  if (T.isFailed(parseData)) {
+    // TODO: 에러처리
+    return {
+      redirect: {
+        destination: PATH.HOME,
+        permanent: true,
+      },
+    }
+  }
+  const { title, content, createdBy, subwayLine } = parseData.result
+
+  return {
+    props: {
+      title,
+      content,
+      createdBy,
+      subwayLine,
+    },
+  }
 }
