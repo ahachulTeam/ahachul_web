f5b1125adbfb1366374da4aba48dffe05e61405d
createhb21 <alsghk9701@gmail.com>
2024-01-15 13:46:23 +0900

    feat: update yarn setting dependencies

--- apps/landing/next.config.js
+++ apps/landing/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@wanted/ui'],
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
