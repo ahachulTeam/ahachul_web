77bff09d4f1d6db2e8db7f2972746828b02822e7
createhb21 <alsghk9701@gmail.com>
2024-01-15 14:02:05 +0900

    feat: implement ahhachul storybook

---
+++ apps/storybook/stories/ReactComponents/Layout/Divider.stories.tsx
@@ -0,0 +1,42 @@
+import "@ahhachul/react-components-layout/style.css";
+import { Box, Divider as _Divider } from "@ahhachul/react-components-layout";
+import { vars } from "@ahhachul/design-system";
+
+export default {
+  title: "React Components/Layout/Divider",
+  component: _Divider,
+  decorators: [
+    (Story) => (
+      <Box padding={3} style={{ width: "300px", height: "300px" }}>
+        <Story />
+      </Box>
+    ),
+  ],
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    orientation: {
+      options: ["horizontal", "vertical"],
+      control: "select",
+    },
+    variant: {
+      options: ["solid", "dashed"],
+      control: "select",
+    },
+    color: {
+      options: Object.keys(vars.colors.$scale),
+      control: "select",
+    },
+  },
+};
+
+export const Divider = {
+  args: {
+    color: "gray",
+    size: 1,
+    variant: "solid",
+    orientation: "horizontal",
+  },
+};
---
+++ apps/storybook/stories/ReactComponents/Layout/Heading.stories.tsx
@@ -0,0 +1,35 @@
+import "@ahhachul/react-components-layout/style.css";
+import { Heading as _Heading } from "@ahhachul/react-components-layout";
+import { classes, vars } from "@ahhachul/design-system";
+
+export default {
+  title: "React Components/Layout/Typography/Heading",
+  component: _Heading,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    as: {
+      options: ["h1", "h2", "h3", "h4", "h5", "h6"],
+      control: "select",
+    },
+    fontSize: {
+      options: Object.keys(classes.typography.heading),
+      control: "select",
+    },
+    color: {
+      options: Object.keys(vars.colors.$scale),
+      control: "select",
+    },
+  },
+};
+
+export const Heading = {
+  args: {
+    as: "h1",
+    children: "Hello World",
+    fontSize: "4xl",
+    color: "gray",
+  },
+};
