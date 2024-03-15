c46e45b0b98052741af88e06f897efa6418f6290
createhb21 <alsghk9701@gmail.com>
2024-03-15 17:40:27 +0900

    feat: add custom hooks

--- services/ahhachul.com/src/hooks/useLoading.ts
+++ services/ahhachul.com/src/hooks/useLoading.ts
@@ -3,14 +3,14 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { loaded, loading } from 'stores/ui';
 
-function useLoading() {
+function useLoading(ms: number = 750) {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loading());
   }, []);
 
-  useTimeout(() => dispatch(loaded()), 700);
+  useTimeout(() => dispatch(loaded()), ms);
 
   return null;
 }
