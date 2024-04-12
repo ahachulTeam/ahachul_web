eb719f920f6668dec8982a9c14b76be624219d62
createhb21 <alsghk9701@gmail.com>
2024-04-12 22:36:28 +0900

    [next.ahhachul.com][FEAT] - signIn api bug fix

--- services/next.ahhachul.com/src/api/auth/index.ts
+++ services/next.ahhachul.com/src/api/auth/index.ts
@@ -1,16 +1,13 @@
-import queryString from 'query-string';
 import { IRefreshTokenParams, IResponse, ISocialSignInParams, ISocialSignInResponse, IToken } from '@/src/types';
 import { API_BASE_URL } from '@/src/data/api';
 import { base } from '..';
 
 export const getAuthURL = `${API_BASE_URL}/auth`;
 
-const login = async (params: ISocialSignInParams) => {
-  const queryParams = queryString.stringify(params);
+const login = async (body: ISocialSignInParams) => {
+  const url = `${getAuthURL}/login?`;
 
-  const url = `${getAuthURL}/logins?${queryParams}`;
-
-  return await base.get<IResponse<ISocialSignInResponse>>(url);
+  return await base.post<IResponse<ISocialSignInResponse>>(url, body);
 };
 
 const refreshToken = (body: IRefreshTokenParams) => base.post<IResponse<IToken>>(`${getAuthURL}/tokens`, body);
