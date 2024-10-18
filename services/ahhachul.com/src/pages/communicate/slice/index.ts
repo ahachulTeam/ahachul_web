bcd13adf7f9a204b11c3d870244edfaae5cf5f52
createhb21 <alsghk9701@gmail.com>
2024-10-19 00:30:50 +0900

    refactor: 필터의 uniqueId는 필터그룹에서 관리하여 이를 사용하는 곳에 강제성을 부여

--- services/ahhachul.com/src/pages/communicate/slice/index.ts
+++ services/ahhachul.com/src/pages/communicate/slice/index.ts
@@ -2,9 +2,7 @@ import { APP_UNIQUE_FILTER_ID_LIST } from 'widgets/filters/data/uniqueId';
 import { createFilterStoreWithPersist } from 'widgets/filters/slice/filters';
 import { COMMUNITY_FILTER_DEFAULT_VALUES } from '../data';
 
-export const communityUniqueFilterId = APP_UNIQUE_FILTER_ID_LIST.Community;
-
 export const useCommunityFilterStore = createFilterStoreWithPersist(
   COMMUNITY_FILTER_DEFAULT_VALUES,
-  communityUniqueFilterId,
+  APP_UNIQUE_FILTER_ID_LIST.Community,
 );
--- services/ahhachul.com/src/pages/lost-found/slice/index.ts
+++ services/ahhachul.com/src/pages/lost-found/slice/index.ts
@@ -2,9 +2,7 @@ import { APP_UNIQUE_FILTER_ID_LIST } from 'widgets/filters/data/uniqueId';
 import { createFilterStoreWithPersist } from 'widgets/filters/slice/filters';
 import { LOST_FOUND_FILTER_DEFAULT_VALUES } from 'pages/lost-found/data';
 
-export const lostFoundUniqueFilterId = APP_UNIQUE_FILTER_ID_LIST.LostFound;
-
 export const useLostFoundFilterStore = createFilterStoreWithPersist(
   LOST_FOUND_FILTER_DEFAULT_VALUES,
-  lostFoundUniqueFilterId,
+  APP_UNIQUE_FILTER_ID_LIST.LostFound,
 );
