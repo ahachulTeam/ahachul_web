567418f2b10d54de1bb1c4a91bf85f33b386a06c
createhb21 <alsghk9701@gmail.com>
2024-10-19 00:32:12 +0900

    refactor: useKeywordSearch 내의 과도한 메모이제이션 제거

--- services/ahhachul.com/src/widgets/filters/lib/useKeywordSearch.ts
+++ services/ahhachul.com/src/widgets/filters/lib/useKeywordSearch.ts
@@ -1,28 +1,21 @@
-import { useMemo, useCallback } from 'react';
 import { useStepFlow } from 'app/stackflow';
 import type { AppUniqueFilterId } from '../model';
 
 export const useKeywordSearch = (uniqueId: AppUniqueFilterId) => {
   const { stepPush } = useStepFlow(uniqueId);
 
-  const updateKeyword = useCallback(
-    (value: string) => {
-      stepPush({
-        keyword: value.trim(),
-      });
-    },
-    [stepPush],
-  );
+  const updateKeyword = (value: string) => {
+    stepPush({
+      keyword: value.trim(),
+    });
+  };
 
-  const resetKeyword = useCallback(() => {
+  const resetKeyword = () => {
     updateKeyword('');
-  }, [updateKeyword]);
+  };
 
-  return useMemo(
-    () => ({
-      updateKeyword,
-      resetKeyword,
-    }),
-    [updateKeyword, resetKeyword],
-  );
+  return {
+    updateKeyword,
+    resetKeyword,
+  };
 };
