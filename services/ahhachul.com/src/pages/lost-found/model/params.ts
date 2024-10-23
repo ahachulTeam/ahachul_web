710980675d7be33e79a7e53660200d8198d235f6
createhb21 <alsghk9701@gmail.com>
2024-10-23 16:42:51 +0900

    feat: change lostFound getList params interface

--- services/ahhachul.com/src/pages/lost-found/model/params.ts
+++ services/ahhachul.com/src/pages/lost-found/model/params.ts
@@ -1,15 +1,14 @@
-import type { PageParams, PageSort } from 'entities/with-server';
+import type { CursorBasedPaginationParams } from 'entities/with-server';
 import { WithArticleId } from 'features/articles';
 import type { WithSubwayLineId } from 'features/subway-lines';
 import type { LostType } from '.';
 
-type LostOriginType = 'LOST112' | 'APP';
-interface LostFoundListParams extends WithSubwayLineId, PageParams {
+interface LostFoundListParams
+  extends WithSubwayLineId,
+    CursorBasedPaginationParams {
   lostPostId: number;
   keyword: string;
-  sort: PageSort;
   lostType: LostType;
-  origin: LostOriginType;
 }
 
 export type ParamsOfLostFoundList = Partial<LostFoundListParams>;
