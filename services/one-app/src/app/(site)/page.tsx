255a6cc4f60762045276656a05e6a34dd5613581
Lulu <63350767+YoonJeongLulu@users.noreply.github.com>
2024-10-30 20:53:30 +0900

    fix test case

--- services/one-app/src/app/(site)/page.tsx
+++ services/one-app/src/app/(site)/page.tsx
@@ -2,11 +2,13 @@ import PlusIcon from '@/common/assets/icons/PlusIcon';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-row bg-slate-50 sm:flex-col gap-3">
-      <h1 className="text-2xl font-bold">App Router</h1>
-      <h5>PC: 정렬 가로</h5>
-      <h5>모바일: 정렬 세로</h5>
-      <PlusIcon />
-    </div>
+    <main className="flex min-h-screen flex-row bg-slate-50 ">
+      <div className='flex flex-row sm:flex-col gap-3'>
+        <h1 className="text-2xl font-bold">App Router</h1>
+        <p>PC: 정렬 가로</p>
+        <p>모바일: 정렬 세로</p>
+        <PlusIcon />
+      </div>
+    </main>
   );
 }
