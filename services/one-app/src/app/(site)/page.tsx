87c85b71b5ad9d6c33aea79e8483d379bdcffdc2
Lulu <63350767+YoonJeongLulu@users.noreply.github.com>
2024-10-30 17:20:19 +0900

    apply dynamically config

--- services/one-app/src/app/(site)/page.tsx
+++ services/one-app/src/app/(site)/page.tsx
@@ -2,9 +2,11 @@ import PlusIcon from '@/common/assets/icons/PlusIcon';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>App Router</h1>
+    <div className="flex min-h-screen flex-row bg-slate-50 sm:flex-col gap-3">
+      <h1 className="text-2xl font-bold">App Router</h1>
+      <h5>PC: 정렬 가로</h5>
+      <h5>모바일: 정렬 세로</h5>
       <PlusIcon />
-    </main>
+    </div>
   );
 }
