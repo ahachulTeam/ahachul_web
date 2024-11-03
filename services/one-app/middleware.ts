2f7f3b40fed2b9e521c88d83465bd5a260bbc62d
Lulu <63350767+YoonJeongLulu@users.noreply.github.com>
2024-11-04 00:04:42 +0900

    fix middleware

--- services/one-app/middleware.ts
+++ services/one-app/middleware.ts
@@ -1,19 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { isBot } from 'next/dist/server/web/spec-extension/user-agent';
-import { CookieKey } from './src/common/constants';
 import { SITE_URL } from './src/common/constants/env';
+import { TokenService } from '@/app/api/util/TokenService';
 
 export function middleware(request: NextRequest) {
   const userAgent = request.headers.get('user-agent');
   if (!userAgent || isBot(userAgent)) return NextResponse.next();
 
-  const accessToken = request.cookies.get(CookieKey.ACCESS_TOKEN);
-  const refreshToken = request.cookies.get(CookieKey.REFRESH_TOKEN);
-
   const { pathname, search } = request.nextUrl;
   const fullPath = `${pathname}${search}`;
 
-  if (!accessToken || !refreshToken) {
+  if (!TokenService.accessToken || !TokenService.refreshToken) {
     return NextResponse.redirect(
       `${SITE_URL}/login?returnTo=${encodeURIComponent(fullPath)}`,
     );
