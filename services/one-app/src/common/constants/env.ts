68bc42cff9b01f75f902d6a33fc2d3e5cce9978e
Lulu <63350767+YoonJeongLulu@users.noreply.github.com>
2024-11-05 23:06:03 +0900

    feat: one-app env (#242)
    * feature one-app env
    
    * fix parsing error
    
    * fix env
    
    * feature site url

--- services/one-app/src/common/constants/env.ts
+++ services/one-app/src/common/constants/env.ts
@@ -1,5 +1,18 @@
-export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL as string;
-export const IS_DEV_ENV = SITE_URL.startsWith('http://localhost'); // 수정하기
+export enum AppEnv {
+  PRODUCTION = 'production',
+  STAGING = 'staging',
+  DEV = 'development',
+}
+
+export const APP_ENV = process.env.NEXT_PUBLIC_APP_ENV || AppEnv.DEV;
+export const IS_DEV_ENV = APP_ENV === AppEnv.DEV;
+
+export const SITE_URL = (() => {
+  if (IS_DEV_ENV) return 'http://localhost:3000';
+  if (APP_ENV === AppEnv.DEV) return process.env.APP_DEV_URL;
+  return process.env.APP_PRODUCTION_URL;
+})();
+
 export const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_MOCKING === 'enabled'
     ? 'http://localhost:9090'
