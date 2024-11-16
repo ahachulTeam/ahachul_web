18fe8f841d26731a4da05a733c98d778d9c9b862
Hyobum Lee <alsghk9701@gmail.com>
2024-11-16 14:36:46 +0900

    feat: ♻️ MSW 관련 함수 리팩토링 (#255)

---
+++ services/one-app/src/mocks/units/user.ts
@@ -0,0 +1,17 @@
+import { http } from 'msw';
+import {
+  User,
+  createErrorResponse,
+  createSuccessResponse,
+} from '../mock-utils';
+
+const mock_내_정보_조회 = http.get('/members', ({ params }) => {
+  const { userId } = params;
+  const found = User.find((v) => v.memberId === userId);
+  if (!found) {
+    return createErrorResponse();
+  }
+  return createSuccessResponse(found);
+});
+
+export default [mock_내_정보_조회];
