1b62dd64df18d542d55dc22a175cbcae0398685c
Lulu <63350767+YoonJeongLulu@users.noreply.github.com>
2024-11-23 21:18:26 +0900

    SVG To direct rendering

--- services/one-app/next.config.mjs
+++ services/one-app/next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, options) {
+    // SVG 파일을 @svgr/webpack으로 처리
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {},
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
