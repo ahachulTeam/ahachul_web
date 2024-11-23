878aa717c458e9f9c362c0e226b477b6c30e41b8
Lulu <63350767+YoonJeongLulu@users.noreply.github.com>
2024-11-23 21:21:51 +0900

    SVG To direct rendering (#263)

--- services/one-app/tailwind.config.ts
+++ services/one-app/tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
