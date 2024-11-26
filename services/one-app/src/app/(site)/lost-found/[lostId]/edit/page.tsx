c230c375808dd8f4e842665582b22f5e5846e24d
holim0 <holim1226@gmail.com>
2024-11-26 21:27:48 +0900

    refactor: 유실물 페이지 라우터 재구성(수정, 생성 구분)  (#258)

---
+++ services/one-app/src/app/(site)/lost-found/[lostId]/edit/page.tsx
@@ -0,0 +1,5 @@
+const LostFoundEditPage = () => {
+  return <h1>수정 페이지</h1>;
+};
+
+export default LostFoundEditPage;
---
+++ services/one-app/src/app/(site)/lost-found/create/page.tsx
@@ -0,0 +1,5 @@
+const LostFoundCreatePage = () => {
+  return <h1>유실물 등록 페이지</h1>;
+};
+
+export default LostFoundCreatePage;
