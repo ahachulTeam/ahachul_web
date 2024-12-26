243c22bdb3febeb420e86db1427bba63f9afffba
holim0 <holim1226@gmail.com>
2024-12-26 18:51:36 +0900

    refactor: adapter 내에 lostId 없을 경우 기본 값으로 리턴되게 설정 (#258)

--- services/one-app/src/app/(site)/lost-found/_hook/useFormAdapter.tsx
+++ services/one-app/src/app/(site)/lost-found/_hook/useFormAdapter.tsx
@@ -2,28 +2,42 @@ import { useMemo } from 'react';
 import { useGetLostFoundDetail } from '../_lib/get';
 import type { LostFoundFormData } from '@/model/LostFound';
 
-type UseLostFoundFormAdapter = (params: {
+type UseLostFoundFormAdapter = ({
+  lostId,
+}: {
   lostId: string;
 }) => LostFoundFormData;
+
+const DEFAULT_DATA: LostFoundFormData = {
+  title: '',
+  initialContent: '',
+  subwayLineId: 1,
+  lostType: 'LOST' as LostFoundFormData['lostType'],
+  images: [],
+} as const;
 
 const useLostFoundFormAdapter: UseLostFoundFormAdapter = ({ lostId }) => {
   const { data } = useGetLostFoundDetail(lostId);
 
-  const result = useMemo(
-    () => ({
-      title: data?.title ?? '',
-      initialContent: data?.content ?? '',
-      subwayLineId: data?.subwayLineId ?? 1,
-      lostType: data?.lostType ?? 'LOST',
+  const result = useMemo(() => {
+    if (!data) {
+      return DEFAULT_DATA;
+    }
+
+    return {
+      title: data.title ?? '',
+      initialContent: data.content ?? '',
+      subwayLineId: data.subwayLineId ?? 1,
+      lostType: data.lostType ?? 'LOST',
       images:
-        data?.images.map((image) => ({
+        data.images.map((image) => ({
           id: image.imageId ?? null,
           data: null,
           url: image.imageUrl,
         })) ?? [],
-    }),
-    [data],
-  );
+    };
+  }, [data]);
+
   return result;
 };
 
