f18f65de738d324314442cf188b304a2a84004c3
Hyobum Lee <alsghk9701@gmail.com>
2025-01-21 18:26:05 +0900

    feat: 토큰 재발급 (#296) (#298)

--- services/ahhachul.com/src/services/user.ts
+++ services/ahhachul.com/src/services/user.ts
@@ -5,14 +5,26 @@ import { useAuth } from '@/contexts';
 
 export const userKeys = {
   all: ['user'] as const,
+  infos: () => [...userKeys.all, 'infos'] as const,
+  stations: () => [...userKeys.all, 'stations'] as const,
 };
 
 export const useFetchUserProfile = () => {
   const { authService } = useAuth();
 
   return useQuery({
-    queryKey: userKeys.all,
+    queryKey: userKeys.infos(),
     enabled: authService.isAuthenticated,
     queryFn: api.fetchUserProfile,
+  });
+};
+
+export const useFetchUserFavoriteStations = () => {
+  const { authService } = useAuth();
+
+  return useQuery({
+    queryKey: userKeys.stations(),
+    enabled: authService.isAuthenticated,
+    queryFn: api.fetchUserFavoriteStations,
   });
 };
