77a27af118ad80add3717246c746948049b1cbd2
createhb21 <alsghk9701@gmail.com>
2025-02-02 18:07:53 +0900

    chore: next.js project에도 bundle analyze와 부하 테스트트 스크립트 추가 (#ci-advancement)

--- services/one-app/next.config.ts
+++ services/one-app/next.config.ts
@@ -1,3 +1,7 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -37,4 +41,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
