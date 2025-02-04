e05d3b96dcdbbd364729a02bc39fa9bb062124c7
createhb21 <alsghk9701@gmail.com>
2025-02-04 14:01:07 +0900

    chore: ci affected test (#ci-adjust)

--- services/ahhachul.com/src/mocks/handlers/example.ts
+++ services/ahhachul.com/src/mocks/handlers/example.ts
@@ -6,7 +6,7 @@ export const handlers = [
   http.get('https://example.com/user', () => {
     // ...and respond to them using this JSON response.
     return HttpResponse.json({
-      id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
+      id: 'c7b3d8e0-5e0b-4b0f-8b3a-qwer1234xx',
       firstName: 'John',
       lastName: 'Maverick',
     });
