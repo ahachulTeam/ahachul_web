7af6bf32fd4d4ca54d6396bc44d8c30a49d49e38
createhb21 <alsghk9701@gmail.com>
2025-05-09 19:57:51 +0900

    FEAT apple login rollback (#develop)

--- services/ahhachul.com/src/components/domain/auth/socialLogin/SocialLogin.component.tsx
+++ services/ahhachul.com/src/components/domain/auth/socialLogin/SocialLogin.component.tsx
@@ -7,19 +7,11 @@ import * as S from './SocialLogin.styled';
 
 const SocialLogin = () => {
   const clickLogin = async (loginType: SocialSignInType) => {
-    if (loginType === SocialSignInType.APPLE) {
-      const APPLE_CLIENT_ID = 'com.ahhachul.todayapp.login';
-      const APPLE_REDIRECT_URI = 'https://app.dev.ahhachul.com/login/callback?type=APPLE';
-      const url = `https://appleid.apple.com/auth/authorize?client_id=${APPLE_CLIENT_ID}&redirect_uri=${APPLE_REDIRECT_URI}&response_type=code&scope=openid&response_mode=form_post`;
-      window.location.assign(url);
-      return;
-    } else {
-      try {
-        const response = await fetchRedirectUrl(loginType);
-        window.location.assign(response.result.redirectUrl);
-      } catch (error) {
-        window.alert('로그인 정보를 불러오는데 실패했어요.');
-      }
+    try {
+      const response = await fetchRedirectUrl(loginType);
+      window.location.assign(response.result.redirectUrl);
+    } catch (error) {
+      window.alert('로그인 정보를 불러오는데 실패했어요.');
     }
   };
 
